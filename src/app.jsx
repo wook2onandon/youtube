310a67790ac57999e171cca25cb6ddb9b72f6b92
wook2onandon <babyldk@gmail.com>
2021-04-30 17:47:30 +0900

    code refactoring

--- src/app.jsx
+++ src/app.jsx
@@ -1,28 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import './app.css';
+import styles from './app.module.css';
 import VideosList from './components/video_list/videos_list';
+import SearchHeader from './components/search_header/search_header';
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
 
-  useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
+  const search = (query) => {
+    youtube
+      .search(query) //
+      .then((videos) => setVideos(videos));
+  };
 
-    fetch(
-      'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCv5MC5IDVhuxXXJRkkIleipPqSTnyPBx4',
-      requestOptions,
-    )
-      .then((response) => response.json())
-      .then((result) => setVideos(result.items))
-      .catch((error) => console.log('error', error));
+  useEffect(() => {
+    youtube
+      .mostPopular() //
+      .then((videos) => setVideos(videos));
   }, []);
   return (
-    <h1>
+    <div className={styles.app}>
+      <SearchHeader onSearch={search} />
       <VideosList videos={videos} />
-    </h1>
+    </div>
   );
 }
 
---
+++ src/service/youtube.js
@@ -0,0 +1,30 @@
+class Youtube {
+  constructor(key) {
+    this.key = key;
+    this.getRequestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+  }
+  mostPopular() {
+    return fetch(
+      'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCv5MC5IDVhuxXXJRkkIleipPqSTnyPBx4',
+      this.getRequestOptions,
+    )
+      .then((response) => response.json())
+      .then((result) => result.items);
+  }
+
+  search(query) {
+    return fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyCv5MC5IDVhuxXXJRkkIleipPqSTnyPBx4`,
+      this.getRequestOptions,
+    )
+      .then((response) => response.json())
+      .then((result) =>
+        result.items.map((item) => ({ ...item, id: item.id.videoId })),
+      );
+  }
+}
+
+export default Youtube;
