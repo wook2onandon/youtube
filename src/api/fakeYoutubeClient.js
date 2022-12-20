9585b0add371bd48ee81db147f35c00dc229fd07
wook2onandon <babyldk@gmail.com>
2022-12-20 18:03:07 +0900

    code refactoring

--- src/api/fakeYoutubeClient.js
+++ src/api/fakeYoutubeClient.js
@@ -1,10 +1,15 @@
 import axios from 'axios';
 
 export default class FakeYoutubeClient {
-  async search() {
-    return axios.get(`/data/search.json`);
+  async search({ params }) {
+    return axios.get(
+      `/data/${params.relatedToVideoId ? 'related' : 'search'}.json`,
+    );
   }
   async videos() {
     return axios.get(`/data/popular.json`);
+  }
+  async channels() {
+    return axios.get(`/data/channel.json`);
   }
 }
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -2,8 +2,6 @@ import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import VideoCard from '../components/VideoCard';
-import Youtube, { search } from '../api/youtube';
-import FakeYoutube from '../api/fakeYoutubeClient';
 import { useYoutubeApi } from '../context/YoutubeApiContext';
 
 export default function Videos() {
@@ -13,14 +11,15 @@ export default function Videos() {
     isLoading,
     error,
     data: videos,
-  } = useQuery(['videos', keyword], () => youtube.search(keyword));
+  } = useQuery(['videos', keyword], () => youtube.search(keyword), {
+    staleTime: 1000 * 60 * 1,
+  });
   return (
     <>
-      <div>Videos {keyword ? `🔍${keyword}` : '🔥'}</div>
       {isLoading && <p>Loading...</p>}
       {error && <p>something is wrong 😖</p>}
       {videos && (
-        <ul>
+        <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 gap-y-4">
           {videos.map((video) => (
             <VideoCard key={video.id} video={video} />
           ))}
